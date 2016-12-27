ec6849b91e6ab9e7c1be1225992d44d2d73cadf8
Lively Magic Architect <1757977307@qq.com>
2016-12-27 20:52:22 +0800

    update

--- js/pay.js
+++ js/pay.js
@@ -1,10 +1,10 @@
-var a=document.getElementById('content_ul').getElementsByTagName('li');
-
-a[0].className='z_nav';
-for(var i=0; i<a.length; i++) {
-	a[i].onmouseover=function(){
-		for (var i=0; i<a.length; i++) {
-			a[i].className='';
+var li = document.getElementById('content_ul').getElementsByTagName('li'),
+	Len = li.length;
+li[0].className='z_nav';
+for(var i=0; i<Len; i++) {
+	li[i].onmouseover=function(){
+		for (var i=0; i<Len; i++) {
+			li[i].className='';
 		};
 		this.className='z_nav';
 	};
